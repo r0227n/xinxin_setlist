85f2d4e28bf631dc0349600fc5e015fc91eba7e7
r0227n <developryo@gmail.com>
2025-08-10 19:39:46 +0900

    feat: セットリスト取得処理を作成

--- app/lib/repositories/music_order_repository.g.dart
+++ app/lib/repositories/music_order_repository.g.dart
@@ -7,12 +7,15 @@ part of 'music_order_repository.dart';
 // **************************************************************************
 
 String _$musicOrderRepositoryHash() =>
-    r'2ccf9a2df863500ab9b78e2bd4fdd50e41c57cd5';
+    r'accc3332386984c1a3f248064db22058b14ac349';
 
 /// See also [MusicOrderRepository].
 @ProviderFor(MusicOrderRepository)
 final musicOrderRepositoryProvider =
-    AsyncNotifierProvider<MusicOrderRepository, List<MusicOrder>>.internal(
+    AutoDisposeAsyncNotifierProvider<
+      MusicOrderRepository,
+      List<MusicOrder>
+    >.internal(
       MusicOrderRepository.new,
       name: r'musicOrderRepositoryProvider',
       debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -22,6 +25,6 @@ final musicOrderRepositoryProvider =
       allTransitiveDependencies: null,
     );
 
-typedef _$MusicOrderRepository = AsyncNotifier<List<MusicOrder>>;
+typedef _$MusicOrderRepository = AutoDisposeAsyncNotifier<List<MusicOrder>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
