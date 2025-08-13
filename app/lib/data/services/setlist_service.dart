66fd2549d9d8b599b4c597ee78c6015b71afb767
r0227n <developryo@gmail.com>
2025-08-13 22:17:32 +0900

    refactor: 未使用の関数&パッケージを削除

--- app/lib/data/services/setlist_service.dart
+++ app/lib/data/services/setlist_service.dart
@@ -1,31 +1,23 @@
 import 'package:app/data/repositories/event_repository.dart';
-import 'package:app/data/repositories/music_repository.dart';
 import 'package:app_logger/app_logger.dart';
 import 'package:cores/cores.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:uuid/uuid.dart';
 
 part 'setlist_service.g.dart';
-
-const _uuid = Uuid();
 
 @riverpod
 SetlistService setlistService(Ref ref) {
   return SetlistService(
-    musicRepository: ref.watch(musicRepositoryProvider.notifier),
     eventRepository: ref.watch(eventRepositoryProvider.notifier),
   );
 }
 
 class SetlistService with LoggerMixin {
   SetlistService({
-    required MusicRepository musicRepository,
     required EventRepository eventRepository,
-  }) : _musicRepository = musicRepository,
-       _eventRepository = eventRepository;
+  }) : _eventRepository = eventRepository;
 
-  final MusicRepository _musicRepository;
   final EventRepository _eventRepository;
 
   /// [eventId] セットリストを作成する対象のイベントID
@@ -46,10 +38,8 @@ class SetlistService with LoggerMixin {
       '${musicIds.length} musics for event ID: $eventId',
     );
 
-    final setlistId = _generateRandomId(20);
-
     return Setlist(
-      id: setlistId,
+      id: targetEvent.id,
       eventId: eventId,
       musicIds: musicIds,
     );
@@ -79,15 +69,5 @@ class SetlistService with LoggerMixin {
     );
 
     return setlists;
-  }
-
-  Future<Music> getMusicByMusicId(String musicId) async {
-    final music = await _musicRepository.get(musicId);
-    return music;
-  }
-
-  /// 指定された長さのランダムな文字列IDを生成する
-  String _generateRandomId(int length) {
-    return _uuid.v4().substring(0, length);
   }
 }
