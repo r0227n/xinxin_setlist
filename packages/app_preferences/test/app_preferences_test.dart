ff5a29125013c3638a2588452d2ca25a5d55cde3
r0227n <developryo@gmail.com>
2025-08-14 07:56:24 +0900

    refactor: testコードの不備を修正

--- packages/app_preferences/test/app_preferences_test.dart
+++ packages/app_preferences/test/app_preferences_test.dart
@@ -2,6 +2,7 @@ import 'package:app_preferences/app_preferences.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
   group('App Preferences', () {
     test('AppPreferences exports all required components', () {
       // Basic test to ensure the package exports are working
--- packages/app_preferences/test/src/widgets/dialogs/locale_selection_dialog_test.dart
+++ packages/app_preferences/test/src/widgets/dialogs/locale_selection_dialog_test.dart
@@ -7,6 +7,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
   group('LocaleOption', () {
     test('creates with required properties', () {
       const option = LocaleOption(
