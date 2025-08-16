59b9e6ae6520a1a85d2ae015717cb9710266709d
r0227n <developryo@gmail.com>
2025-08-17 01:11:11 +0900

    refactor: ShareButton をいい感じに修正

--- app/lib/pages/widgets/share_button.dart
+++ app/lib/pages/widgets/share_button.dart
@@ -8,40 +8,61 @@ export 'package:share_plus/share_plus.dart' show XFile;
 
 class ShareButton extends StatelessWidget {
   const ShareButton({
-    this.icon = const Icon(Icons.share),
     super.key,
-    this.getFiles,
-    this.tooltip,
-    this.onShareCompleted,
-    this.text,
-    this.url,
-  });
+    Widget icon = const Icon(Icons.share),
+    List<XFile>? Function()? getFiles,
+    String? tooltip,
+    VoidCallback? onShareCompleted,
+    String? text,
+    String? subject,
+    String? url,
+  }) : _icon = icon,
+       _getFiles = getFiles,
+       _tooltip = tooltip,
+       _onShareCompleted = onShareCompleted,
+       _text = text,
+       _subject = subject,
+       _url = url;
 
-  final Widget icon;
-  final String? tooltip;
-  final List<XFile>? Function()? getFiles;
-  final VoidCallback? onShareCompleted;
-  final String? text;
-  final String? url;
+  /// 共有ボタンに表示するアイコンウィジェット
+  final Widget _icon;
+
+  /// ボタンのツールチップテキスト
+  final String? _tooltip;
+
+  /// 共有するファイルを取得する関数
+  final List<XFile>? Function()? _getFiles;
+
+  /// 共有完了時に呼び出されるコールバック関数
+  final VoidCallback? _onShareCompleted;
+
+  /// 共有するテキスト内容
+  final String? _text;
+
+  /// 共有時の件名
+  final String? _subject;
+
+  /// 共有するURL
+  final String? _url;
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: () => _handleShare(context),
-      icon: icon,
-      tooltip: tooltip,
+      icon: _icon,
+      tooltip: _tooltip,
     );
   }
 
   Future<void> _handleShare(BuildContext context) async {
-    final files = getFiles?.call();
+    final files = _getFiles?.call();
 
     await SharePlus.instance.share(
       ShareParams(
         files: files,
-        text: text,
-        subject: text,
-        uri: url != null ? Uri.parse(url!) : null,
+        text: _text,
+        subject: _subject,
+        uri: _url != null ? Uri.parse(_url) : null,
         sharePositionOrigin: !kIsWeb && Platform.isIOS
             ? _getShareButtonRect(context)
             : null,
@@ -49,45 +70,23 @@ class ShareButton extends StatelessWidget {
     );
 
     if (context.mounted) {
-      onShareCompleted?.call();
+      _onShareCompleted?.call();
     }
   }
 
   Rect? _getShareButtonRect(BuildContext context) {
-    try {
-      final renderBox = context.findRenderObject() as RenderBox?;
-      if (renderBox == null || !renderBox.hasSize) {
-        return null;
-      }
-
-      final size = renderBox.size;
-      final position = renderBox.localToGlobal(Offset.zero);
-
-      return Rect.fromCenter(
-        center: position + Offset(size.width / 2, size.height / 2),
-        width: size.width,
-        height: size.height,
-      );
-    } on Exception catch (error) {
-      debugPrint('Failed to get share button rect: $error');
+    final renderBox = context.findRenderObject() as RenderBox?;
+    if (renderBox == null || !renderBox.hasSize) {
       return null;
     }
-  }
 
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties.add(StringProperty('tooltip', tooltip));
-    properties.add(
-      ObjectFlagProperty<List<XFile>? Function()?>.has('getFiles', getFiles),
+    final size = renderBox.size;
+    final position = renderBox.localToGlobal(Offset.zero);
+
+    return Rect.fromCenter(
+      center: position + Offset(size.width / 2, size.height / 2),
+      width: size.width,
+      height: size.height,
     );
-    properties.add(
-      ObjectFlagProperty<VoidCallback?>.has(
-        'onShareCompleted',
-        onShareCompleted,
-      ),
-    );
-    properties.add(StringProperty('text', text));
-    properties.add(StringProperty('url', url));
   }
 }
