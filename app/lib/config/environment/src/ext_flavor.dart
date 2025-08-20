a7062b5855b7fbbd5d68b6a0fde51650faea72c8
r0227n <developryo@gmail.com>
2025-08-20 13:36:16 +0900

    refactor: ignore lint ext_flavor.dart

--- app/lib/config/environment/src/ext_flavor.dart
+++ app/lib/config/environment/src/ext_flavor.dart
@@ -1,11 +1,9 @@
 // CI環境でlintが失敗するため、ignore_for_fileを使用している
-// ignore_for_file: uri_does_not_exist
+// ignore_for_file: type=lint
 
 import 'package:app/config/environment/src/flavor.dart';
 import 'package:app/config/environment/src/production_firebase_options.dart'
     as prod;
-import 'package:app/config/environment/src/staging_firebase_options.dart'
-    as stg;
 import 'package:firebase_core/firebase_core.dart';
 
 extension FlavorFirebaseOptions on Flavor {
