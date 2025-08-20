3036e1df48b5854d9650f18ef6715ae849faaf4c
Ryo24 <40818362+r0227n@users.noreply.github.com>
2025-08-20 11:07:42 +0900

    GAの導入 (#38)
    * chore: add firebase packages
    
    * feat: firebase 関連のファイルをignoreに設定
    
    * feat: FLAVOR から環境情報を取得し、firebase_optionsを切り替え可能にした
    
    * chore: diff gen
    
    * feat: Firebaseの初期化&GA登録処理を実装
    
    * feat: Add Firebase setup tasks and script for multiple flavors
    
    * feat: Enhance GitHub Actions workflow with Firebase setup for production
    
    * refactor: Update Firebase setup in GitHub Actions workflow to create configuration files directly
    
    * chore: fix typo
    
    * chore: update dart_define sample.json
    
    * chore: Suppress error output when creating configuration files in GitHub Actions workflow
    
    * chore: Update GitHub Actions workflow to use heredoc for creating configuration files
    
    * chore: Add staging Firebase options and suppress lint errors in ext_flavor.dart
    
    * chore: Refactor flavor initialization and clean up lint suppressions in environment files

--- app/lib/router/app_router.dart
+++ app/lib/router/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:app/router/routes.dart';
+import 'package:app/services/firebase_analytics_service.dart';
 import 'package:app_logger/app_logger.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -11,11 +13,13 @@ part 'app_router.g.dart';
 @riverpod
 GoRouter appRouter(Ref ref) {
   final talker = ref.watch(talkerProvider);
+  final analytics = ref.watch(firebaseAnalyticsProvider);
 
   return GoRouter(
     debugLogDiagnostics: kDebugMode,
     observers: [
       TalkerRouteObserver(talker),
+      FirebaseAnalyticsObserver(analytics: analytics),
     ],
     initialLocation: '/',
     routes: $appRoutes,
